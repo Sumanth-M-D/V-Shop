389063594e04f946108f4cc25f53b1bfa33c5888
Sumanth-M-D <sumanthmdevadiga67@gmail.com>
2024-10-27 14:29:42 +0530

    added deployed BASE_URL of server to client

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,2 +1,4 @@
-export const BASE_URL = "http://localhost:3000/api";
+// export const BASE_URL = "http://localhost:3000/api";
+
+export const BASE_URL = "https://v-shop-j48d.onrender.com";
 export const ROWS_PER_PAGE = 1;
