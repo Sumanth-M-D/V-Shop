194d7fba789afb75efefea548b85d02e1f5292f5
Sumanth-M-D <sumanthmdevadiga67@gmail.com>
2024-10-27 14:32:09 +0530

    corrrected the BASE_URL in client

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,4 +1,4 @@
 // export const BASE_URL = "http://localhost:3000/api";
 
-export const BASE_URL = "https://v-shop-j48d.onrender.com";
+export const BASE_URL = "https://v-shop-j48d.onrender.com/api";
 export const ROWS_PER_PAGE = 1;
