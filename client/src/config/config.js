6f7ef576eb140a00dc08888769289472f3236937
Sumanth-M-D <sumanthmdevadiga67@gmail.com>
2025-01-29 08:53:36 +0530

    added new server url to client config

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,6 +1,12 @@
 // export const BASE_URL = "http://localhost:3000/api";
 
+// Sumanth-M-D
 // export const BASE_URL = "https://v-shop-j48d.onrender.com/api";
 
-export const BASE_URL = "https://v-shop-7rvi.onrender.com/api";
+// Sumanth-M-D2
+// export const BASE_URL = "https://v-shop-7rvi.onrender.com/api";
+
+// Sumanth-M-D3
+export const BASE_URL = "https://v-shop-aiob.onrender.com";
+
 export const ROWS_PER_PAGE = 1;
