c746d218f74c6bf48b41846a5f59b25c633117e6
Sumanth-M-D <sumanthmdevadiga67@gmail.com>
2025-01-29 10:11:58 +0530

    fix: vit.config file

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    cssCodeSplit: false, // Ensures CSS is bundled in the output
+  },
   base: "/",
 });
