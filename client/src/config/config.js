e6cd14ad7765b75081bd9c36e082eb9b97083364
Sumanth-M-D <sumanthmdevadiga67@gmail.com>
2025-02-01 12:06:09 +0530

    changed server back to 2nd

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -4,9 +4,9 @@
 // export const BASE_URL = "https://v-shop-j48d.onrender.com/api";
 
 // Sumanth-M-D2
-// export const BASE_URL = "https://v-shop-7rvi.onrender.com/api";
+export const BASE_URL = "https://v-shop-7rvi.onrender.com/api";
 
 // Sumanth-M-D3
-export const BASE_URL = "https://v-shop-aiob.onrender.com/api";
+// export const BASE_URL = "https://v-shop-aiob.onrender.com/api";
 
 export const ROWS_PER_PAGE = 1;
